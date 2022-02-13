75fc88f263307006e4d2062568c812d98db3b40d
Sudhanshu Sharma <shudhanshu0511max@gmail.com>
2022-02-13 22:45:33 +0530

    Sign up UI done

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ import Signup from "./Signup";
 const App = () => {
   return (
     <div>
-      <Login />
-      {/* <Signup /> */}
+      {/* <Login /> */}
+      <Signup />
       <Footer />
     </div>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,55 @@
 import React from "react";
-import { Box, Button, Typography } from "../Imports/imports";
+import { Box, Button, TextField, Link } from "../Imports/imports";
 
 const Signup = () => {
   return (
     <>
       <Box className="container">
-        <h2>Sign Up</h2>
+        <h1>Sign Up</h1>
+        <TextField
+          sx={{
+            marginBottom: 3,
+          }}
+          variant="outlined"
+          type="email"
+          label="Email"
+          fullWidth
+          placeholder="example@xyz.com"
+          autoComplete="email"
+        />
+        <TextField
+          sx={{
+            marginBottom: 3,
+          }}
+          variant="outlined"
+          type="password"
+          label="Password"
+          fullWidth
+          placeholder="*******"
+        />
+        <TextField
+          sx={{
+            marginBottom: 2,
+          }}
+          variant="outlined"
+          type="password"
+          label="Re-enter Password"
+          fullWidth
+          placeholder="*******"
+        />
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{
+            marginBottom: 3,
+          }}
+        >
+          Sign up
+        </Button>
+        <Link href="#" underline="none">
+          Already have an account? <u>Login</u>
+        </Link>
       </Box>
     </>
   );
